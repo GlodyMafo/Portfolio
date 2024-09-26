840e39f85312cc270aa735776791eaf9f3f0b4ed
Glody Mafo <glodymafokis@gmail.com>
2024-09-26 14:22:08 -0700

    new update

---
+++ src/Components/portfolio/data/projects.js
@@ -0,0 +1,77 @@
+const projects = [
+    {
+      title: "Cmb menuiserie",
+      category: "Web Application",
+      imageUrl: "/cmbsite.png",
+      demoUrl: "https://cmb-menuiserie.com/",
+      githubUrl: "#"
+    },
+  
+    {
+      title: "Josephine Files Converter",
+      category: "Web Application",
+      imageUrl: "/jfsconv.png",
+      demoUrl: "https://josephinefc.onrender.com",
+      githubUrl: "https://github.com/GlodyMafo/jsfcov"
+    },
+    {
+      title: "Contact Book",
+      category: "Web Application",
+      imageUrl: "/contactBook.png",
+      demoUrl: "https://glodymafo.github.io/Projet_Carnet_de_Contacts/",
+      githubUrl: "https://github.com/GlodyMafo/Carnet-de-contact"
+    },
+    {
+      title: "GaRi Exposition",
+      category: "Web Application",
+      imageUrl: "/CarExpo.png",
+      demoUrl: "",
+      githubUrl: "https://github.com/GlodyMafo/CarExpo"
+    },
+  
+    {
+      title: "KBO Prod Logo",
+      category: "Branding",
+      imageUrl: "/KboProd.png",
+    },
+  
+    {
+      title: "CMB Logo",
+      category: "Branding",
+      imageUrl: "/cmbLogo.png",
+    },
+    {
+      title: "Collection dans l'ame Logo",
+      category: "Branding",
+      imageUrl: "/CollectionDansLaame.png",
+    },
+    // {
+    //   title: "Cumorah Hill Logo",
+    //   category: "Branding",
+    //   imageUrl: "/CollectionDansLaame.png",
+    // },
+    {
+      title: "243 Direct News Logo",
+      category: "Branding",
+      imageUrl: "/243News.png",
+    },
+    {
+      title: "Congo Umoja Logo",
+      category: "Branding",
+      imageUrl: "/CongoUmoja.png",
+    },
+    {
+      title: "Salon de l'environnment Flyer",
+      category: "Graphic Design",
+      imageUrl: "/projet_salon-Récupéré_105930.jpg",
+    },
+    {
+      title: "LDS conference Katuba stake Flyer",
+      category: "Graphic Design",
+      imageUrl: "/JKL_085900.png",
+    },
+  
+  ];
+
+ 
+ export default projects 
